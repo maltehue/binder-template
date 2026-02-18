43cf67cc17d398719603d129bbd5ccca5ba3b07c
huerkamp <huerkamp@uni-bremen.de>
2026-02-18 15:44:39 +0100

    source giskard workspace

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,6 +3,7 @@
 # Launch the ROS2
 source ${ROS_PATH}/setup.bash
 source ${MV_ROOT}/Multiverse-Launch/src/multiverse_connectors/multiverse_ros_connector/ros_ws/multiverse_ws2/install/setup.bash
+source ${MV_ROOT}/../giskard_ws/install/setup.bash
 # Add other startup programs here
 
 # The following line will allow the binderhub start Jupyterlab, should be at the end of the entrypoint.
